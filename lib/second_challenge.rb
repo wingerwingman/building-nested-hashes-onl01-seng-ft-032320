79b6538dafc6046302cff20c5c32ded85e5bb78e
Justin Winger <justinwinger4@gmail.com>
2020-03-26 18:13:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch {},
-      :matriarch {},
-      :hero {},
-      :hero_friends {},
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends {}
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch {},
+      
       }}
 
   
